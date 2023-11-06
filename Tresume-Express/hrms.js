7c19cbd7c2c88518f75a8ed6710c54b3ca43cdd9
Venkat-Tresume <venkat@tresume.us>
2023-11-06 22:14:10 +0530

    HRMS-List

--- Tresume-Express/hrms.js
+++ Tresume-Express/hrms.js
@@ -32,13 +32,12 @@ const dbConfig = {
   trustServerCertificate: true,
 };
 
-const traineeID = '27315';
 router.post('/getInterviewList', async (req, res) => {
   try {
     const pool = await sql.connect(dbConfig);
-    const traineeID = '27315';
+    
 
-    const query = "SELECT CONVERT(NVARCHAR, TI.InterviewDate, 101) AS Date, CONCAT(T1.FirstName, ' ', T1.LastName) AS Marketer, ISNULL(TI.Assistedby, '') AS Assigned, TI.InterviewMode, ISNULL(TI.Notes, '') AS Notes, ISNULL(TI.ClientName, '') AS Client, ISNULL(TI.VendorName, '') AS Vendor, ISNULL(TI.SubVendor, '') AS SubVendor, ISNULL(TI.TypeofAssistance, '') AS TypeofAssistance FROM TraineeInterview TI LEFT JOIN Trainee T1 ON T1.TraineeID = TI.RecruiterID WHERE TI.active = 1 AND TI.TraineeID = "+traineeID+" ORDER BY TI.CreateTime DESC;";
+    const query = "SELECT CONVERT(NVARCHAR, TI.InterviewDate, 101) AS Date, CONCAT(T1.FirstName, ' ', T1.LastName) AS Marketer, ISNULL(TI.Assistedby, '') AS Assigned, TI.InterviewMode, ISNULL(TI.Notes, '') AS Notes, ISNULL(TI.ClientName, '') AS Client, ISNULL(TI.VendorName, '') AS Vendor, ISNULL(TI.SubVendor, '') AS SubVendor, ISNULL(TI.TypeofAssistance, '') AS TypeofAssistance FROM TraineeInterview TI LEFT JOIN Trainee T1 ON T1.TraineeID = TI.RecruiterID WHERE TI.active = 1 AND TI.TraineeID = '20742' ORDER BY TI.CreateTime DESC;";
 console.log(query);
     const result = await pool.request()
       .query(query);
