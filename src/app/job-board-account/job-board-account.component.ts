46c97bfa2bc6e7640972a48c3a206c36d7367b7d
Mariya-Sherine <141424695+Mariya-Sherine@users.noreply.github.com>
2023-12-19 03:59:48 +0530

    Insert Job Board

--- src/app/job-board-account/job-board-account.component.ts
+++ src/app/job-board-account/job-board-account.component.ts
@@ -1,5 +1,11 @@
-import { Component} from '@angular/core';
-
+import { Component, OnInit} from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+// import { JobBoardAccountService } from './job-board-account.service';
+import { JobBoardAccountService } from './job-board-account.service'
+import { CookieService } from 'ngx-cookie-service';
+import { MessageService } from 'primeng/api';
+import { AppService } from '../app.service';
+import { ReviewService } from '../review-tresume/review.service';
 interface Account {
   accountName: string;
   status: string;
@@ -9,9 +15,13 @@ interface Account {
 @Component({
   selector: 'app-job-board-account',
   templateUrl: './job-board-account.component.html',
-  styleUrls: ['./job-board-account.component.scss']
+  styleUrls: ['./job-board-account.component.scss'],
+  providers: [CookieService, ReviewService, MessageService,AppService,JobBoardAccountService],
 })
-export class JobBoardAccountComponent{
+export class JobBoardAccountComponent implements OnInit {
+
+  constructor(private formBuilder: FormBuilder, private service:JobBoardAccountService) { 
+  }
  
   options: string[] = ['Dice', 'Monster', 'CareerBuilder'];
 
@@ -23,25 +33,55 @@ export class JobBoardAccountComponent{
     this.passwordInputType = this.showPassword ? 'text' : 'password';
   }
 
+  jobBoard:any;
   accountName: string = '';
   selectedJobBoard: string = '';
   accounts: Account[] = [];
 
-  addAccount() {
-    const newAccount: Account = {
-      accountName: this.accountName,
-      selectedJobBoard:this.options,
-      status: 'active'  
-    };
-    this.accounts.push(newAccount);
-  }
+  // addAccount() {
+  //   const newAccount: Account = {
+  //     accountName: this.accountName,
+  //     selectedJobBoard:this.options,
+  //     status: 'active'  
+  //   };
+  //   this.accounts.push(newAccount);
+  // }
 
   deleteAccount(index: number) {
     this.accounts.splice(index, 1);
   }
 
+  ngOnInit(): void {
+    this.jobBoard = this.formBuilder.group({
+    accountName: ['', [Validators.required, Validators.minLength(3)]],
+    fromDate: [''],
+    toDate: [''],
+    username: ['', [Validators.required, Validators.minLength(3)]],
+    password: ['', [Validators.required, Validators.minLength(3)]],
+    });
 
-  
+  }
 
+  addAccount() {
+    // const newAccount: Account = {
+    //   accountName: this.accountName,
+    //   selectedJobBoard:this.options,
+    //   status: 'active'  
+    // };
+    // this.accounts.push(newAccount);
+    let Req = {
+      accountName: this.jobBoard.value.accountName,
+      fromDate: this.jobBoard.value.fromDate,
+      toDate: this.jobBoard.value.toDate,
+      zipRecruiter: this.jobBoard.value.zipRecruiter,
+      username: this.jobBoard.value.username,
+      password: this.jobBoard.value.password,
+    };
+
+    console.log(Req);
+    this.service.insertJobBoardAccountList(Req).subscribe((x: any) => {
+      console.log(x);
+    });
+  }
  
 }
