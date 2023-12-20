fc80cb39317dd16fa9e8e3cf7cff59535886412a
Venkat-Tresume <venkat@tresume.us>
2023-12-20 22:56:24 +0530

    Forget-Password

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -145,6 +145,7 @@ import { DiceComponent } from './landing-page/dice/dice.component';
 import { JobleeComponent } from './landing-page/joblee/joblee.component';
 import { OptNationComponent } from './landing-page/opt-nation/opt-nation.component';
 import { YahooComponent } from './landing-page/yahoo/yahoo.component';
+import { ForgetPasswordComponent } from './forget-password/forget-password.component';
 
 
 
@@ -232,6 +233,7 @@ import { YahooComponent } from './landing-page/yahoo/yahoo.component';
     JobleeComponent,
     OptNationComponent,
     YahooComponent,
+    ForgetPasswordComponent,
   ],
   imports: [
     HttpClientModule,
