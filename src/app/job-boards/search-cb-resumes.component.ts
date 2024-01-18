00ec18426a7ef7d0c31270d287aab4cce4c08461
wilsontresume <balasudharsan@tresume.us>
2024-01-18 23:44:20 +0530

    loader for jobboards

--- src/app/job-boards/search-cb-resumes.component.ts
+++ src/app/job-boards/search-cb-resumes.component.ts
@@ -226,6 +226,7 @@ export class SearchResumesCBComponent implements OnInit {
     }
 
     ngOnInit(): void {
+        this.loading = true;
         this.cookieValue = this.cookieService.get('userName1')
         //division
         this.OrgID = this.cookieService.get('OrgID');
@@ -670,6 +671,7 @@ export class SearchResumesCBComponent implements OnInit {
                                 divid = x.result[0].id;
                                 resolve();
                             }
+                            this.loading = false;
                         })
                         .catch((error: any) => {
                             reject(error);
