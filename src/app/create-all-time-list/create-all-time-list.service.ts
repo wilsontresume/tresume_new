188d483fa159e3aef32824ed3b4877e775168b9a
Mariya-Sherine <141424695+Mariya-Sherine@users.noreply.github.com>
2024-01-30 21:50:30 +0530

    Updates_timesheet

--- src/app/create-all-time-list/create-all-time-list.service.ts
+++ src/app/create-all-time-list/create-all-time-list.service.ts
@@ -7,34 +7,34 @@ import { Observable } from 'rxjs';
 @Injectable()
 
 export class CreateAllTimeListService {
-    public endpoint = environment.apiUrl;
-    //public endpoint = 'https://alpha.tresume.us/TresumeAPI/';
-    // public endpoint = 'http://localhost:3000/';
-    
+  public endpoint = environment.apiUrl;
+  //public endpoint = 'https://alpha.tresume.us/TresumeAPI/';
+  // public endpoint = 'http://localhost:3000/';
 
-    constructor(private http: HttpClient) { }
-createTimesheet(data: any) {
-return this.http.post(this.endpoint, data);
-}
 
-getTimesheetCandidatetList(request: any): Observable<ResponseDetails> {
-  return this.http.post<ResponseDetails>(this.endpoint + 'getTimesheetCandidatetList', request);
-}
+  constructor(private http: HttpClient) { }
+  createTimesheet(data: any) {
+    return this.http.post(this.endpoint, data);
+  }
 
-getCreateProjectList(request: any): Observable<ResponseDetails> {
-  return this.http.post<ResponseDetails>(this.endpoint + 'getCreateProjectList', request);
-}
-getPayItemList(request: any): Observable<ResponseDetails> {
-  return this.http.post<ResponseDetails>(this.endpoint + 'getPayItemList', request);
-}
-getLocationList(request: any): Observable<ResponseDetails> {
-  return this.http.post<ResponseDetails>(this.endpoint + 'getLocationList', request);
-}
-deletetimesheetdata(request: any): Observable<ResponseDetails> {
-  return this.http.post<ResponseDetails>(this.endpoint + 'deletetimesheetdata', request);
-}
+  getTimesheetCandidatetList(request: any): Observable<ResponseDetails> {
+    return this.http.post<ResponseDetails>(this.endpoint + 'getTimesheetCandidatetList', request);
+  }
+
+  getCreateProjectList(request: any): Observable<ResponseDetails> {
+    return this.http.post<ResponseDetails>(this.endpoint + 'getCreateProjectList', request);
+  }
+  // getPayItemList(request: any): Observable<ResponseDetails> {
+  //   return this.http.post<ResponseDetails>(this.endpoint + 'getPayItemList', request);
+  // }
+  getLocationList(request: any): Observable<ResponseDetails> {
+    return this.http.post<ResponseDetails>(this.endpoint + 'getLocationList', request);
+  }
+  deletetimesheetdata(request: any): Observable<ResponseDetails> {
+    return this.http.post<ResponseDetails>(this.endpoint + 'deletetimesheetdata', request);
+  }
 }
 export interface ResponseDetails {
-    flag?: any;
-    result?: any;
+  flag?: any;
+  result?: any;
 }
