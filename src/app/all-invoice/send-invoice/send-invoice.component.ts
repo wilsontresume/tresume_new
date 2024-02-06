f991dfc60d3619477ac678ab8ebf4c0dc933ac93
Balasudharsan64 <146992011+Balasudharsan64@users.noreply.github.com>
2024-02-06 20:20:47 +0530

    Update all-invoice

--- src/app/all-invoice/send-invoice/send-invoice.component.ts
+++ src/app/all-invoice/send-invoice/send-invoice.component.ts
@@ -6,10 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./send-invoice.component.scss']
 })
 export class SendInvoiceComponent implements OnInit {
+  showPopup: boolean = false;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+  selectFile(inputId: string): void {
+    document.getElementById(inputId)?.click();
+  }
+
+  handleFileChange(event: any): void {
+    // Handle the file change event here
+    const selectedFile = event.target.files[0];
+    console.log(selectedFile);
+  }
+  togglePopup(event: Event): void {
+    event.preventDefault();
+    this.showPopup = !this.showPopup;
+  }
+
+  closePopup(): void {
+    this.showPopup = false;
   }
 
 }
