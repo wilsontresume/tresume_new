833e74fe7d3887397a8b33d5f61cf4113c101a4c
Mariya-Sherine <141424695+Mariya-Sherine@users.noreply.github.com>
2024-02-23 03:41:57 +0530

    updates

--- Tresume-Express/timesheet.js
+++ Tresume-Express/timesheet.js
@@ -167,7 +167,7 @@ router.post("/getCompletedTimesheetResult", async (req, res) => {
       var request = new sql.Request();
 
       var query =
-        "SELECT CONCAT(t.firstname, ' ', t.lastname) as Candidate, TM.fromdate, TM.todate, TM.totalhrs, TM.created_at, TM.status, TM.comments, TM.details FROM Timesheet_Master TM INNER JOIN Trainee T ON TM.traineeid = T.traineeid WHERE T.timesheet_admin ='" + req.body.traineeID + "' AND TM.status = 1";
+        "SELECT CONCAT(t.firstname, ' ', t.lastname) as Candidate, TM.fromdate, TM.todate, TM.totalhrs, TM.created_at, TM.status, TM.comments, TM.details FROM Timesheet_Master TM INNER JOIN Trainee T ON TM.traineeid = T.traineeid WHERE T.timesheet_admin ='" + req.body.traineeID + "' AND TM.status = 3";
 
       console.log(query);
       request.query(query, function (err, recordset) {
