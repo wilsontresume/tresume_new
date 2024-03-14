bb338a977553974a079145d10d9e06998ebdaedf
Francy Romanuse <francy@tresume.us>
2024-03-14 18:48:45 +0530

    jobposting

--- src/app/applicant-details/applicant-details.component.ts
+++ src/app/applicant-details/applicant-details.component.ts
@@ -1,29 +1,67 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CookieService } from 'ngx-cookie-service';
+import { ApplicantDetailsService } from './applicant-details.service';
+import { MessageService } from 'primeng/api';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-applicant-details',
   templateUrl: './applicant-details.component.html',
+  providers: [CookieService, ApplicantDetailsService, MessageService],
   styleUrls: ['./applicant-details.component.scss']
 })
 export class ApplicantDetailsComponent{
-  loading:boolean = false;
-
-  applicants: any[] = [
-    { date: '12/10/2023', name: 'Wilson', jobTitle: 'Senior Developer', legalStatus: 'Approved', payRate: '$ 500',source:'abcde' },
-    { date: '13/10/2023', name: 'Bala', jobTitle: 'Junior Developer', legalStatus: 'Approved', payRate: '$ 400',source:'fghij' },
-    { date: '14/10/2023', name: 'Francy', jobTitle: 'Junior Developer', legalStatus: 'Approved', payRate: '$ 300',source:'klmno' },
-    { date: '15/10/2023', name: 'Maria', jobTitle: 'Junior Developer', legalStatus: 'Approved', payRate: '$ 200',source:'pqrst' },
-  ];
-
+  
+  loading: boolean = false;
+  applicants: any[] = [];
   displayedApplications: number = this.applicants.length;
   totalApplications: number = this.applicants.length;
-  
+  noResultsFound: boolean = false;
+  TraineeID: string = '';
+  authType: any;
+  OrgID:string = '';
+  JobID:any;
+  isAdmin: string;
+
+  constructor(
+    private cookieService: CookieService,
+    private service: ApplicantDetailsService,
+    private messageService: MessageService,
+    private router: Router,
+    private route: ActivatedRoute,
+  ) {
+    this.JobID = this.route.snapshot.params["jobID"];
+  }
+
+  ngOnInit(): void {
+    this.loading = true;
+    this.TraineeID = this.cookieService.get('TraineeID');
+    this.OrgID = this.cookieService.get('OrgID');
+    this.isAdmin = this.cookieService.get('IsAdmin');
+    this.fetchjobapplicants();
+  }
+
   performSearch(searchTerm: string) {
     this.applicants = this.applicants.filter(applicant =>
       applicant.name.toLowerCase().includes(searchTerm.toLowerCase())
     );
 
     this.displayedApplications = this.applicants.length;
+  }
+
+  fetchjobapplicants() {
+    let Req = {
+      TraineeID: this.TraineeID,
+      OrgID:this.OrgID,
+      JobID:this.JobID,
+      isAdmin:this.isAdmin,
+
+    };
+    this.service.getjobapplicants(Req).subscribe((x: any) => {
+      this.loading = false;
+      this.applicants = x.result;
+      console.log(this.applicants);
+      // this.noResultsFound = this.applicants.length === 0;
+    });
   }
 }
